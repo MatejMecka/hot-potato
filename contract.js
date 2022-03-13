9fa460114ad58cc822c83e3b56eba0c693f196e5
Glitch (glitch-hello-node) <none>
2022-03-13 17:47:09 +0000

    🏭😀 Checkpoint ./contract.js:437136/100175 ./LICENSE.md:437136/1072

--- contract.js
+++ contract.js
@@ -159,18 +159,6 @@ const {
                 source: NFT_ASSET.issuer,
               })
             );
-
-            transaction.addOperation(
-                Operation.setTrustLineFlags({
-                  // This is the first authorization open operation for the new NFT allowing it to be minted from the issuing account to the mint/royalty user account
-                  trustor: person_who_holds_the_potato,
-                  asset: WINNER_NFT_ASSET,
-                  flags: {
-                    authorized: true,
-                  },
-                  source: NFT_ASSET.issuer,
-                })
-              );
   
             transaction.addOperation(
               Operation.manageSellOffer({
@@ -195,15 +183,6 @@ const {
             );
   
             transaction.addOperation(
-              Operation.payment({
-                asset: WINNER_NFT_ASSET,
-                amount: "0.0000001",
-                destination: person_who_holds_the_potato,
-                source: NFT_ASSET.issuer,
-              })
-            );
-  
-            transaction.addOperation(
               Operation.setTrustLineFlags({
                 // Now that the payment for the NFT has been made we close the authorization effectively locking down the NFT into the user account where they may now hold the NFT but not sell it unless they do so through the authorized/official `offer.js` contract
                 trustor: person_who_holds_the_potato,
@@ -218,7 +197,7 @@ const {
             transaction.addOperation(
               Operation.manageData({
                 name: "currentAccount",
-                value: person_who_gets_the_potato,
+                value: person_who_holds_the_potato,
                 source: NFT_ASSET.issuer,
               })
             );
@@ -332,9 +311,9 @@ const {
                 // 6. Send Winner NFT
                 transaction.addOperation(
                     Operation.changeTrust({
-                      asset: NFT_ASSET,
+                      asset: WINNER_NFT_ASSET,
                       limit: "1",
-                      source: person_who_gets_the_potato,
+                      source: person_who_holds_the_potato,
                     })
                   );
 
