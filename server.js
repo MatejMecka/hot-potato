950f9cf816993986b14bc735a4b5cc76df9cba1e
Glitch (glitch-hello-node) <none>
2022-03-13 14:42:21 +0000

    🏬👮 Checkpoint ./public/complete_pass.html:437136/1924 ./server.js:437136/2061

--- server.js
+++ server.js
@@ -49,28 +49,7 @@ if (seo.url === "glitch-default") {
 * Returns src/pages/index.hbs with data built into it
 */
 fastify.get("/", function(request, reply) {
-  
-  // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
-  
-  // If someone clicked the option for a random color it'll be passed in the querystring
-  if (request.query.randomize) {
-    
-    // We need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
-    
-    // Add the color properties to the params object
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo
-    };
-  }
-  
-  // The Handlebars code will be able to access the parameter values and build them into the page
-  reply.view("/src/pages/index.hbs", params);
+  reply.view("/public/index.html",);
 });
 
 /**
@@ -114,49 +93,12 @@ fastify.get("/generate_xdr", async function(request, reply){
   
 })
 
-fastify.post("/", function(request, reply) {
-  
-  // Build the params object to pass to the template
-  let params = { seo: seo };
-  
-  // If the user submitted a color through the form it'll be passed here in the request body
-  let color = request.body.color;
-  
-  // If it's not empty, let's try to find the color
-  if (color) {
-    // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
-    
-    // Load our color data file
-    const colors = require("./src/colors.json");
-    
-    // Take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-    
-    // Now we see if that color is a key in our colors object
-    if (colors[color]) {
-      
-      // Found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo
-      };
-    } else {
-      
-      // No luck! Return the user value as the error property
-      params = {
-        colorError: request.body.color,
-        seo: seo
-      };
-    }
-  }
-  
-  // The Handlebars template will use the parameter values to update the page with the chosen color
-  reply.view("/src/pages/index.hbs", params);
-});
-
 fastify.get('/timeline', function(request,reply){
   reply.view("/public/timeline.html")
+})
+
+fastify.get('/complete-pass', function(request,reply){
+  reply.view("/public/complete_pass.html")
 })
 
 fastify.get('/.well-known/stellar.toml', function(request,reply){
